c7ff0289919c45700f650153960fabbab9e29df9
cdog <i@cdog.me>
2021-12-23 18:33:43 +0800

    Use BotMsgReq instead of separated structs.

--- server/type.go
+++ server/type.go
@@ -25,22 +25,32 @@ func (result *Result) getPicPath(index uint) string {
 }
 
 type Setu struct {
-	Pid    int      `json:"pid"`
-	P      int      `json:"p"`
-	Uid    int      `json:"uid"`
-	Title  string   `json:"title"`
-	Author string   `json:"author"`
-	Url    string   `json:"url"`
-	R18    bool     `json:"r18"`
-	Width  int      `json:"width"`
-	Height int      `json:"height"`
-	Tags   []string `json:"tags"`
+	Pid     int      `json:"pid"`
+	P       int      `json:"p"`
+	Uid     int      `json:"uid"`
+	Title   string   `json:"title"`
+	Author  string   `json:"author"`
+	Url     string   `json:"url"`
+	R18     bool     `json:"r18"`
+	Width   int      `json:"width"`
+	Height  int      `json:"height"`
+	Tags    []string `json:"tags"`
 	DumpUrl string
 }
 
-type PostWeChatNews struct {
-	MsgType string `json:"msgtype"`
-	News    News   `json:"news"`
+type BotMsgType string
+
+const (
+	BotMsgText  BotMsgType = "text"
+	BotMsgNews  BotMsgType = "news"
+	BotMsgImage BotMsgType = "image"
+)
+
+type BotMsgReq struct {
+	MsgType BotMsgType `json:"msgtype"`
+	News    *News      `json:"news,omitempty"`
+	Image   *Image     `json:"image,omitempty"`
+	Text    *Text      `json:"text,omitempty"`
 }
 
 type News struct {
@@ -54,19 +64,9 @@ type Article struct {
 	Picurl      string `json:"picurl"`
 }
 
-type PostWeChatPic struct {
-	MsgType string `json:"msgtype"`
-	Image   Image  `json:"image"`
-}
-
 type Image struct {
 	Base64 string `json:"base64"`
 	Md5    string `json:"md5"`
-}
-
-type PostWeChatText struct {
-	MsgType string `json:"msgtype"`
-	Text    Text   `json:"text"`
 }
 
 type Text struct {
